7fdb6ddb7889da3e854e11d6cf61d6bf281a51e4
Alexander Jacks and Patrick Lipscomb <alexander+patrick@>
2016-07-19 15:33:04 -0700

    builds callback to handle broken associations between products and purchases

--- lib/purchase.rb
+++ lib/purchase.rb
@@ -1,3 +1,13 @@
 class Purchase < ActiveRecord::Base
   has_many(:product)
+  after_destroy(:reset_product_associates)
+
+private
+  define_method(:reset_product_associates) do
+    products = self.product()
+    products.each() do |product|
+      product.update({:purchase_id => nil})
+    end
+  end
+
 end
